d537fcbe4f8346a49e6a8dd1f4a6e7a8cc8de573
Jo'rabek <egamovj90@gmail.com>
2023-11-28 18:01:26 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FetchCountries from './components/FetchCountries'
+
+function App() {
+  
+  return (
+    <>
+      <FetchCountries/>
+    </>
+  )
+}
+
+export default App
